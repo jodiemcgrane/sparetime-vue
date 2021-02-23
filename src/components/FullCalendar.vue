a4c1c81744322bbb6bcc74f040bf9033636e98f4
jodiemcgrane <jodiemcgrane101@gmail.com>
2021-02-23 09:22:35 +0000

    Imported FullCalendar.

---
+++ src/components/FullCalendar.vue
@@ -0,0 +1,55 @@
+<!--
+@Date:   2021-02-23T09:10:09+00:00
+@Last modified time: 2021-02-23T09:15:46+00:00
+-->
+<template>
+  <div>
+    <Fullcalendar
+    :options="calendarPlugins"
+    />
+  </div>
+</template>
+
+<script>
+
+import Fullcalendar from '@fullcalendar/vue'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import interactionPlugin from '@fullcalendar/interaction'
+import listPlugin from '@fullcalendar/list'
+
+  export default {
+    components: {
+      Fullcalendar
+    },
+    data() {
+      return {
+        calendarPlugins: {
+          plugins: [
+            dayGridPlugin,
+            timeGridPlugin,
+            interactionPlugin,
+            listPlugin
+          ],
+          headerToolbar: {
+            left: 'prev,today,next',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,timeGridDay'
+          },
+          buttonText: {
+            today: 'Today',
+            month: 'Month',
+            week: 'Week',
+            day: 'Day'
+          },
+          selectable: true,
+          initialView: 'dayGridMonth',
+        }
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,22 @@
 <!--
 @Date:   2021-02-18T16:18:41+00:00
-@Last modified time: 2021-02-22T11:43:15+00:00
+@Last modified time: 2021-02-23T09:16:21+00:00
 -->
-
 <template>
 <div class="home">
-  <img alt="Vue logo" src="../assets/logo.png">
-  <br>
-  <h1 class="display-4">Home</h1>
+  <v-container>
+        <FullCalendar />
+  </v-container>
 </div>
 </template>
 
 <script>
+import FullCalendar from "../components/FullCalendar"
+
 export default {
   name: 'Home',
   components: {
-
+    FullCalendar,
   }
 }
 </script>
